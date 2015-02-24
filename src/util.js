7cf10a399d6e045459be357b53b793dbc867136e
Mudit Ameta <zeusdeux@gmail.com>
2015-02-25 04:37:50 +0530

    [1.0.0] Moved util functions into their own file

---
+++ src/util.js
@@ -0,0 +1,13 @@
+// check if arg is an object
+// isObject :: a -> Bool
+exports.isObject = function isObject(arg) {
+  return typeof arg === 'object' && arg !== null;
+};
+
+// get iterator and object returned by iterator
+// getIteratorAndObj :: (Iterable a) => a -> Object
+exports.getIteratorAndObj = function getIteratorAndObj(iterable){
+  var xsIt = iterable[Symbol.iterator]();
+  var itObj = xsIt.next();
+  return {xsIt, itObj};
+};
