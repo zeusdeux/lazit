e6160e7e455c3ba2af6a8f57ef27d00e319eb639
Mudit Ameta <zeusdeux@gmail.com>
2015-04-15 02:11:28 -0400

    [1.0.0] unzipN logic fixed

--- src/esnext/unzipN.js
+++ src/esnext/unzipN.js
@@ -2,15 +2,18 @@ var replicate = require('./replicate');
 
 // unzipN :: [(a, b, .... , n)] -> ([a], [b], ...., [n])
 function unzipN(a) {
-  var res = [...replicate(a.length, [])];
+  var res;
+
   for (let x of a) {
+    res = res || [...replicate(x.length, [])];
     for (let y of x) {
       let curr = res.shift();
       curr.push(y);
       res.push(curr);
     }
   }
-  return res;
+
+  return res || [];
 }
 
 
