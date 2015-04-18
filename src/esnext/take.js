9b57ef76047415ace47279d6a875317841f81b42
Mudit Ameta <zeusdeux@gmail.com>
2015-04-18 17:36:28 -0400

    [1.0.0-rc.2] Fixed take running n+1 times bug

--- src/esnext/take.js
+++ src/esnext/take.js
@@ -4,7 +4,17 @@ let cu = require('auto-curry');
 // take :: Int -> [a] -> [a]
 function* take(n, a) {
   for (let x of a) {
-    if (n--) yield x;
+    if (n > 1) {
+      yield x;
+      n--;
+    }
+    // this condition makes sure that we yield only
+    // exactly n times and not n + 1 times which
+    // would happen if we were using the old if (n--) code
+    else if (n === 1) {
+      yield x;
+      break;
+    }
     else break;
   }
 }
